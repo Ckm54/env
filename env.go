9aa89154c3136aa772241518e9180b4a54d232e9
ckm54 <mutaic84@gmail.com>
2025-01-22 22:32:34 +0300

    docs: add some documentation for the String function

--- env.go
+++ env.go
@@ -21,6 +21,22 @@ func init() {
 	envs = make([]envVar, 0)
 }
 
+// String registers a new string environment variable with the specified parameters.
+// It appends the variable configuration to the global envs slice and returns a pointer
+// to the string value that will be populated when environment variables are parsed.
+//
+// Parameters:
+//   - name: The environment variable name to look for
+//   - required: Whether this environment variable must be set
+//   - defaultValue: Value to use if environment variable is not set
+//   - help: Description of this environment variable for documentation
+//
+// Example usage:
+//
+//	port := env.String("PORT", false, "8080", "HTTP server port")
+//
+// The returned pointer will be populated with the environment variable value
+// after calling env.Parse()
 func String(name string, required bool, defaultValue, help string) *string {
 	v := new(string)
 
